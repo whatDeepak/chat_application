b6ea115f8bcb384b88b87534898efba06995a230
Deepak <deekay9357@gmail.com>
2023-07-23 21:32:42 +0530

    chat-app@0.0.1

---
+++ src/components/ChatFeed.jsx
@@ -0,0 +1,78 @@
+import MyMessage from './MyMessage';
+import TheirMessage from './TheirMessage';
+import MessageForm from './MessageForm';
+import React, { useState, useEffect } from 'react';
+
+const ChatFeed = (props) => {
+    const [currentDateTime, setCurrentDateTime] = useState(new Date());
+
+  useEffect(() => {
+    // Update the current date and time every second
+    const interval = setInterval(() => {
+      setCurrentDateTime(new Date());
+    }, 1000);
+
+    // Clean up the interval when the component is unmounted
+    return () => clearInterval(interval);
+  }, []);
+
+
+  const { chats, activeChat, userName, messages } = props;
+
+  const chat = chats && chats[activeChat];
+  console.log(chat);
+
+  const renderReadReceipts = (message, isMyMessage) => chat.people.map((person, index) => person.last_read === message.id && (
+    <div
+      key={`read_${index}`}
+      className="read-receipt"
+      style={{
+        float: isMyMessage ? 'right' : 'left',
+        backgroundImage: person.person.avatar && `url(${person.person.avatar})`,
+      }}
+    />
+  ));
+
+  const renderMessages = () => {
+    const keys = Object.keys(messages);
+
+    return keys.map((key, index) => {
+      const message = messages[key];
+      const lastMessageKey = index === 0 ? null : keys[index - 1];
+      const isMyMessage = userName === message.sender.username;
+
+      return (
+        <div key={`msg_${index}`} style={{ width: '100%' }}>
+          <div className="message-block">
+            {isMyMessage
+              ? <MyMessage message={message} />
+              : <TheirMessage message={message} lastMessage={messages[lastMessageKey]} />}
+          </div>
+          <div className="read-receipts" style={{ marginRight: isMyMessage ? '18px' : '0px', marginLeft: isMyMessage ? '0px' : '68px' }}>
+            {renderReadReceipts(message, isMyMessage)}
+          </div>
+        </div>
+      );
+    });
+  };
+
+  if (!chat) return <div />;
+
+  return (
+    <div className="chat-feed">
+      <div className="chat-title-container">
+        <div className="chat-title">{chat?.title}</div>
+        <div className="chat-subtitle">
+            {currentDateTime.toLocaleString()}
+        </div>
+      </div>
+      {renderMessages()}
+      <div style={{ height: '100px' }} />
+      <div className="message-form-container">
+        <MessageForm {...props} chatId={activeChat} />
+      </div>
+    </div>
+  );
+};
+
+export default ChatFeed;
